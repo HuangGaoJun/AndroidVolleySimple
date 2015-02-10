b98d646192135a6408ec9ed4a57453072c0bd091
huanggaojun <97248159@qq.com>
2015-02-10 08:06:49 +0800

    AndroidVolley Test

---
+++ app/src/main/java/com/example/androidvolley/Api/VolleyApi.java
@@ -0,0 +1,14 @@
+package com.example.androidvolley.Api;
+
+/**
+ * Created by Jun on 2015/2/10.
+ */
+public class VolleyApi {
+    public static final String BAIDU = "http://www.baidu.com/";
+
+    public static final String JSON_TEST = "http://echo.jsontest.com/key/value/one/two";
+
+    public static final String POST_TEST = "http://ave.bolyartech.com/params.php";
+
+    public static final String GSON_TEST = "http://validate.jsontest.com/?json={'key':'value'}";
+}
--- app/src/main/java/com/example/androidvolley/ui/SimpleRequetActivity.java
+++ app/src/main/java/com/example/androidvolley/ui/SimpleRequetActivity.java
@@ -1,7 +1,44 @@
 package com.example.androidvolley.ui;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.example.androidvolley.Api.VolleyApi;
+import com.example.androidvolley.R;
+
+
 /**
  * Created by Jun on 2015/2/9.
  */
-public class SimpleRequetActivity {
+public class SimpleRequetActivity extends BaseActivity {
+    private TextView mTvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_request);
+
+        Button btnRequest = (Button)findViewById(R.id.btn_simple_request);
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executeRequest(new StringRequest(Request.Method.GET, VolleyApi.BAIDU,
+                        responseListener(),errorListener()));
+            }
+        });
+    }
+
+    private Response.Listener<String> responseListener(){
+        return new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                mTvResult.setText(s);
+            }
+        };
+    }
 }
